577ccff3568eb2fefc77d16e3c8cbb99f251ddaa
Jack Wiltshire <jackwiltshire.webdev@gmail.com>
2022-09-29 17:00:39 +1000

    Add environment variables

---
+++ app.js
@@ -0,0 +1,85 @@
+//Requirement/Dependencies
+
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+//App set: Express
+
+const app = express();
+
+//Database Connection.
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+//Database Schema's and Model's - Encrypted
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
+
+
+const User = new mongoose.model("User", userSchema);
+
+//Requirement Settings
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Routes
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.route("/login")
+    .get(function(req,res){
+        res.render("login");
+    })
+    .post(function(req,res){
+        const userName = req.body.username;
+        const password = req.body.password;
+        User.findOne({email: userName}, function(err, foundUser){
+            if (err) {
+                console.log(err);
+            } else {
+                if(foundUser) {
+                    if(foundUser.password === password){
+                        res.render("secrets");
+                    }
+                }
+            }
+        });
+    });
+
+app.route("/register")
+    .get(function(req,res){
+        res.render("register");
+    })
+    .post(function(req,res){
+        const newUser = new User ({
+            email: req.body.username,
+            password: req.body.password,
+        });
+        newUser.save(function(err){
+            if(!err) {
+                res.render("secrets");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+
+//Server Connection
+
+app.listen(3000, function(req,res){
+    console.log("Successfully started server on port 3000");
+});
