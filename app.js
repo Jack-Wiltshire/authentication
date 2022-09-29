373b05c340f5e68e9a0bb099e9623fbb9bfa18f6
Jack Wiltshire <jackwiltshire.webdev@gmail.com>
2022-09-29 17:44:27 +1000

    added level 3 security

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 //App set: Express
 
@@ -21,9 +21,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +42,7 @@ app.route("/login")
     })
     .post(function(req,res){
         const userName = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
         User.findOne({email: userName}, function(err, foundUser){
             if (err) {
                 console.log(err);
@@ -66,7 +63,7 @@ app.route("/register")
     .post(function(req,res){
         const newUser = new User ({
             email: req.body.username,
-            password: req.body.password,
+            password: md5(req.body.password),
         });
         newUser.save(function(err){
             if(!err) {
